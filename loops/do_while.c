93e81dc1478afef512dda3baaba5a6da6cb0a039
Jatin Dhiman <jatindhiman31218@gmail.com>
2026-09-14 00:21:13 +0530

    Add files via upload

---
+++ loops/do_while.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main ()
+
+{
+int n;
+int i;
+i=1;
+printf(" no of times you want to print - ");
+scanf("%d", &n);
+do{
+    printf(" value of i is %d\n",i);
+    i++;
+}while (i<=n);
+{
+    /* code */
+}
+
+
+return 0;
+
+}
