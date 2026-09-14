4f92f1fee4a1eabc114f6551b43bb603c8ceadc4
Jatin Dhiman <jatindhiman31218@gmail.com>
2026-09-15 00:52:23 +0530

    Add area calculation for rectangle in area.c

--- Basics of c/area.c
+++ Basics of c/area.c
@@ -1,3 +1,18 @@
 #include<stdio.h>
 
- 
+ int main(){
+
+int l,b,area;
+
+printf("ENTER LENGTH - ");
+scanf("%d", &l);
+
+printf("ENTER BREADTH - ");
+scanf("%d", &b);
+
+area = l*b;
+
+printf(" AREA OF RECTANGLE IS %d", area);
+
+return 0;
+ }
